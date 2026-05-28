eddff16603fcc694632ead544c455dfccda357f0
Erasmo Bezerra <erasmo.ads.tech@gmail.com>
2026-05-28 14:17:00 -0300

    add matrizes

---
+++ 11_matrizes/matrizes.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+    
+    int mat[3][3] = {{1,2,4}, 
+                     {4,5,6}, 
+                     {7,8,9}};
+    
+    printf("Imprimindo a primeira linha:\n");
+    for (int j = 0; j < 3; j++)
+    {
+        printf("%d ", mat[0][j]);
+    }
+
+    printf("\nImprimindo a matriz toda:\n");
+    for (int i = 0; i < 3; i++)
+    {        
+        for (int j = 0; j < 3; j++)
+        {
+            printf("%d ", mat[i][j]);
+        }
+        printf("\n");            
+    }
+    
+    
+
+
+}
